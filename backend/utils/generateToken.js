b4a4e994406c1623bb93dc0bafedf80656eb28fe
unknown <nikhilkachi68@gmail.com>
2025-12-02 22:26:31 +0530

    update token gen file

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -11,7 +11,6 @@ const generateToken = (res, userId) => {
     httpOnly: true,
     secure: true,                   
     sameSite: "none",
-    domain: ".onrender.com",
     path: "/",                                
     maxAge: 3 * 24 * 60 * 60 * 1000,          
   });
